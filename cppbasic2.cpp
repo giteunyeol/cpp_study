185839dd6d7757e2e4ec8d1a85d645e6064352b4
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2024-10-04 20:34:19 +0900

    Add files via upload

---
+++ cppbasic2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    
+    char ch;
+    cout << "문자 입력: ";
+    cin >> ch;
+
+    cout << "입력받은 문자 : " << ch << endl;
+    cout << "입력받은 문자의 아스키코드 : " << (int)ch << endl;
+    
+
+
+
+
+    return 0;
+}
